a8247fcdac63d5a0ad5b2661adddd19568111b7c
mahpah <mahpahh@gmail.com>
2022-09-21 17:49:15 +0700

    add more og tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,13 @@ const Home: NextPage<{
         <title>Share to you</title>
         <meta name="description" content="Something" />
         <meta name="og:image" content={image} />
+        <meta name="og:title" content="Share to you" />
+        <meta name="og:description" content="Some description" />
+        <meta name="og:site_name" content="Levellll" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Share to you" />
+        <meta name="twitter:description" content="Some description" />
+        <meta name="twitter:image" content={image} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </div>
